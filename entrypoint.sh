8bfb16f2858f5b91fd79d8cc18d6b858ec2595d7
Philippe Jandot <fil@cessfil.net>
2024-01-22 17:35:02 +0100

    harden sshd

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,11 @@ mkdir -p ${SSH_HOST_KEYS_DIR}
 # Function to generate a host key if it doesn't exist
 generate_key_if_not_exist() {
   key_path=$1
-  key_type=$2
+  key_bits=$2
+  key_type=$3
 
   if [ ! -f ${key_path} ]; then
-    ssh-keygen -t ${key_type} -f ${key_path} -N ''
+    ssh-keygen -t ${key_type} -b ${key_bits} -f ${key_path} -N ''
     echo "${key_type} key generated at ${key_path}."
   else
     echo "${key_type} key already exists at ${key_path}."
@@ -22,9 +23,8 @@ generate_key_if_not_exist() {
 }
 
 # Generate all necessary keys if they don't exist
-generate_key_if_not_exist "${SSH_HOST_KEYS_DIR}/ssh_host_rsa_key" rsa
-generate_key_if_not_exist "${SSH_HOST_KEYS_DIR}/ssh_host_ecdsa_key" ecdsa
-generate_key_if_not_exist "${SSH_HOST_KEYS_DIR}/ssh_host_ed25519_key" ed25519
+generate_key_if_not_exist "${SSH_HOST_KEYS_DIR}/ssh_host_rsa_key" 4096 rsa
+generate_key_if_not_exist "${SSH_HOST_KEYS_DIR}/ssh_host_ed25519_key" 256 ed25519
 
 cd ${GIT_SERVER_STORE}
 mkdir -p client-keys
